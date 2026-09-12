24981cc0d17866194e824d78fdee7659621e935c
Prince Kumar <princek908442@gmail.com>
2026-09-12 10:46:16 +0530

    Two Sum - 2026-09-12T05:16:15.731Z

---
+++ 1-two-sum/solution.java
@@ -0,0 +1,17 @@
+// 45 ms | 47.2 MB
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+}
+
+        
+    
