11cc716f11c59950f82e79227eb4a2ec6b85ede2
Prince Kumar <princek908442@gmail.com>
2026-09-13 14:08:30 +0530

    Subarray Product Less Than K - 2026-09-13T08:38:26.864Z

---
+++ 713-subarray-product-less-than-k/solution.java
@@ -0,0 +1,18 @@
+// 392 ms | 49.2 MB
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int product = 1;
+            for (int j = i; j < nums.length; j++) {
+                product = product * nums[j];
+                if (product < k) {
+                    count++;
+                } else {
+                    break;
+                }
+            }        }
+
+        return count;
+    }
+}
