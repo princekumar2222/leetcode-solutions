e7b3f71ae70b7501fbf2b7109611de03047b42ed
Prince Kumar <princek908442@gmail.com>
2026-09-13 14:01:49 +0530

    Max Consecutive Ones III - 2026-09-13T08:31:46.667Z

---
+++ 1004-max-consecutive-ones-iii/solution.java
@@ -0,0 +1,19 @@
+// 4 ms | 52.3 MB
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int max=0;int s=0;int e=0; int z=0;
+        while(e<nums.length){
+            int num=nums[e];
+            if(num==0)z++;
+            while(z>k){
+                int st=nums[s];
+                if(st==0)
+                    z--;
+                s++;               
+            }        
+            int size=e-s+1;
+            max=Math.max(max,size);        
+            e++;        }
+        return max;        
+    }
+}
