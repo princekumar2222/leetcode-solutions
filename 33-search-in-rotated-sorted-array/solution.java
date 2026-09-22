207d56b593ef1a8deddd26a5d99f8c0416e8c80b
Prince Kumar <princek908442@gmail.com>
2026-09-22 20:30:12 +0530

    Search in Rotated Sorted Array - 2026-09-22T15:00:09.037Z

---
+++ 33-search-in-rotated-sorted-array/solution.java
@@ -0,0 +1,12 @@
+// 0 ms | 43.6 MB
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
