1802ee8a895db06a845d6feb3b7ff03fe02befc9
Prince Kumar <princek908442@gmail.com>
2026-09-26 10:27:59 +0530

    Count Commas in Range II - 2026-09-26T04:57:58.697Z

---
+++ 3871-count-commas-in-range-ii/solution.java
@@ -0,0 +1,12 @@
+// 1 ms | 42.8 MB
+class Solution {
+    public long countCommas(long n) {
+        long count = 0;
+
+        for(long i=1000;i<=n;i=i* 1000){
+            count=count+n-i+1;
+        }
+
+        return count;
+    }
+}
